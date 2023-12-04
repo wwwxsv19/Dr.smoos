ea485d781711abce3e702b3a03f5028a08aeccb9
kimsiyeon0223 <ksyeon0223@gmail.com>
2023-12-04 23:22:39 +0900

    chore : 경로 수정

--- index.py
+++ index.py
@@ -1,40 +1,49 @@
 from flask import Flask, request, render_template, jsonify
-import connectDB, buzzer
+import connectDB, buzzer, servo
+import time
 
 app = Flask(__name__)
 ddbb = connectDB.connectDB()
 bz = buzzer.Buzzer()
+sv = servo.Servo()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("templates\main\index.html")
 
-@app.route('/signUp')
+
+@app.route("/signUp")
 def signUpInfor():
     data = request.get_json()
-    name = data.get('''''') # js 의 key 이름 삽입할 것!
-    
-    fingerPrint = 1 # 연결된 지문 인식 센서로부터 정보를 받아오는 코드
-    
+    name = data.get("""""")  # js 의 key 이름 삽입할 것!
+
+    fingerPrint = 1  # 연결된 지문 인식 센서로부터 정보를 받아오는 코드
+
     if fingerPrint or name is not None:
         result = ddbb.helloNew(name, fingerPrint)
         return result
     else:
-        return jsonify({'msg':'등록 실패'})
+        return jsonify({"msg": "등록 실패"})
 
-@app.route('/openTheDoor')
+
+@app.route("/openTheDoor")
 def takeInfor():
     data = request.get_json()
-    name = data.get('''''') # js 의 key 이름 삽입할 것!
+    name = data.get("""""")  # js 의 key 이름 삽입할 것!
 
     # fingerPrint = # 연결된 지문 인식 센서로부터 정보를 받아오는 코드
 
     if ddbb.whoAreYou(fingerPrint):
         bz.Welcome()
-        return jsonify({'msg':'환영합니다!'})
+        sv.control_servo(90)
+        time.sleep(2000)
+        sv.control_servo(0)
+        return jsonify({"msg": "문이 정상 작동했습니다"})
     else:
         bz.Emergency
-        return jsonify({'msg':'미등록 사용자 출입 시도 감지'})
+        return jsonify({"msg": "미등록 사용자 출입 시도 감지"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
