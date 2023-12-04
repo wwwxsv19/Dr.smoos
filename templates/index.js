6fcd1dd5ec59b9746a1eb0016fe6d5c874b61736
kimsiyeon0223 <ksyeon0223@gmail.com>
2023-12-04 23:23:02 +0900

    chore : js 파일 작성

---
+++ templates/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $("#name--input").click(function () {
+    let name = $("#name--input").val(); // input 요소에서 key 값을 가져옵니다.
+
+    $.ajax({
+      url: "index.py", // index.py 파일의 경로를 입력해주세요.
+      method: "POST", // POST 방식으로 요청을 보냅니다.
+      data: { key: name }, // key 값을 서버로 전송합니다.
+      success: function (response) {
+        // 서버로부터 성공적으로 응답을 받았을 때 실행될 코드를 작성해주세요.
+        alert("등록 완료!");
+        console.log(response);
+      },
+      error: function (error) {
+        // 서버와의 통신 중 에러가 발생했을 때 실행될 코드를 작성해주세요.
+        window.location.href = "error.html"; // 에러가 발생했을 때 error.html 페이지로 이동합니다.
+        console.log(error);
+      },
+    });
+  });
+});
